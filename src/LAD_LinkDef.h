d90d8350694927e166c1c95e5b8ed779e5d7053e
LucasEhinger <LucasEhinger@gmail.com>
2024-08-21 12:50:13 -0400

    Add relevant libSBSdig files (compiles)

--- src/LAD_LinkDef.h
+++ src/LAD_LinkDef.h
@@ -10,5 +10,14 @@
 #pragma link C++ class THcLADHodoscope+;
 #pragma link C++ class THcLADHodoPlane+;
 #pragma link C++ class THcLADHodoHit+;
+#pragma link C++ class SBSSimADC+;
+#pragma link C++ class SBSSimTDC+;
+#pragma link C++ class SBSSimDataDecoder+;
+#pragma link C++ class SBSSimDecoder+;
+#pragma link C++ class SBSSimEvent+;
+#pragma link C++ class SBSSimFile+;
+#pragma link C++ class genrp_tree_digitized+;
+#pragma link C++ class gep_tree_digitized+;
+#pragma link C++ class gmn_tree_digitized+;
 
 #endif
